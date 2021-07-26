3e631f33a98f9eb539a78a19254e4f4fa8cd5eea
stenioas <stenioas@gmail.com>
2021-07-25 22:53:18 -0300

    feat(productlist): productlist component created

---
+++ src/components/ProductsList/styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+	width: 100%;
+	max-width: 900px;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-end;
+
+	margin: 0 auto;
+`;
