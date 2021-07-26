30cab46a000881138ef4e0fb922fb673b5bdfe2b
stenioas <stenioas@gmail.com>
2021-07-25 22:58:46 -0300

    refactor(app): components and routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Switch as Routes } from "react-router-dom";
+import Header from "./components/Header";
+import NewClient from "./components/NewClient";
+import NewProduct from "./components/NewProduct";
+import Home from "./pages/Home";
+
+import InventoryProvider from "./contexts/InventoryContext";
+
 function App() {
-	return <div className="App">Testando</div>;
+	return (
+		<BrowserRouter>
+			<InventoryProvider>
+				<Header />
+				<Routes>
+					<Route path="/" exact component={Home} />
+					<Route path="/newclient" component={NewClient} />
+					<Route path="/newproduct" component={NewProduct} />
+				</Routes>
+			</InventoryProvider>
+		</BrowserRouter>
+	);
 }
 
 export default App;
